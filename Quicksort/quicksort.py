58db8c8362ddf9e472884261b220e4e925e3273a
廖weber <55630451+weberliao@users.noreply.github.com>
2019-10-22 10:55:06 +0800

    Update quicksort.py

--- Quicksort/quicksort.py
+++ Quicksort/quicksort.py
@@ -1,12 +1,13 @@
-def quicksort(a):
-    left = []
-    right = []
-    middle = []
+def quicksort(list):
+    left = [ ]
+    right = [ ]
+    middle = [ ]
     
-    if len(a)>1:
-        
-        pivot = a[0]
-        for i in a:
+    if len(list)>1:
+        #if條件式只當len裡的數值只剩1個時，停止分類且return
+        pivot = list[0] #設第一個數為基準點避免 index out of range
+
+        for i in list:
             if i < pivot:
                 left.append(i)
             elif i == pivot:
@@ -14,6 +15,6 @@ def quicksort(a):
             else:
                 right.append(i)
     else:
-        return a
+        return list
 
     return quicksort(left) + middle + quicksort(right)
