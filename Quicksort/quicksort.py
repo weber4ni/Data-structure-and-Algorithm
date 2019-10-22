28352ef62acd1eb2f6ab89ccff909d27432c938f
廖weber <55630451+weberliao@users.noreply.github.com>
2019-10-22 14:00:08 +0800

    Update quicksort.py

--- Quicksort/quicksort.py
+++ Quicksort/quicksort.py
@@ -3,16 +3,15 @@ def quicksort(list):
     right = [ ]
     middle = [ ]
     
-    if len(list)>1:
-        #if條件指當len裡的數值只剩1個時，停止分類且return
-        pivot = list[0] #設第一個數為基準點避免 index out of range
+    if len(list)>1:     #if條件指當len裡的數值只剩1個時，停止分類且return
+        pivot = list[0]    #設第一個數為基準點，避免若剩下1個數字時 index out of range
 
         for i in list:
-            if i < pivot:
+            if i < pivot:     #qiucksort概念，把數值小的放置左邊
                 left.append(i)
-            elif i == pivot:
+            elif i == pivot:   #若抓到的數字與基準點相同，先抓出來
                 middle.append(i)
-            else:
+            else:                # 其餘的值放置右邊
                 right.append(i)
     else:
         return list
