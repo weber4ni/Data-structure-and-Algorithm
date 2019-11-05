d05a95889298352940ad7e85745ea7f5d379e5ec
廖weber <55630451+weberliao@users.noreply.github.com>
2019-11-05 14:37:10 +0800

    Update Merge_sort_06170138.py

--- HW2/Merge_sort_06170138.py
+++ HW2/Merge_sort_06170138.py
@@ -1,1 +1,8 @@
-
+ #### merge_sort_06170138.py
+class solution(object):
+	def merge_sort(self, nums):
+                     if len(nums) <=1 :
+                             return nums
+                        middle = 2/len(nums)
+                        leftlen = middle
+                        rightlen = len(nums)-leftlen
