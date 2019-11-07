d2f2cedc38f3fd7e8c3d0c30e66ddfe51bcc2dee
廖weber <55630451+weberliao@users.noreply.github.com>
2019-11-08 01:32:36 +0800

    Update Merge_sort_06170138.py

--- HW2/Merge_sort_06170138.py
+++ HW2/Merge_sort_06170138.py
@@ -1,8 +1,23 @@
  #### merge_sort_06170138.py
-class solution(object):
-	def merge_sort(self, nums):
-                     if len(nums) <=1 :
-                             return nums
-                        middle = 2/len(nums)
-                        leftlen = middle
-                        rightlen = len(nums)-leftlen
+class Solution():
+    def mergeList(self,left, right):# self功用連結mege_sort、left、right
+         if len(left) == 0:#如果左邊沒有值
+             return right#回傳右邊
+         if len(right) == 0:
+             return left
+         if left[0] <= right[0]:#合併完後left、right的第一個值比大小，較小的放入左邊數列，且回傳到left
+             return [left[0]]+self.mergeList(left[1:], right)
+         if left[0] >= right[0]: #與上段相反，且還回傳到right[]
+             return [right[0]]+self.mergeList(left, right[1:])
+
+    def merge_sort(self,nums):
+         if len(nums)<=1:#如果list只有一個值，直接回傳
+             return nums
+         mid = len(nums)//2#找出中間值
+         left = nums[:mid]# left值=0~mid
+         right = nums[mid:]#right值=mid~-1
+
+         leftData = self.merge_sort(left)#把leftdata裡的值藉由merge_sort、mergeList連結起來
+         rightData = self.merge_sort(right)#同上
+
+         return self.mergeList(leftData, rightData)#回傳2值
