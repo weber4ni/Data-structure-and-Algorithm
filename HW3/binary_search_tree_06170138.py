f490579b0fcad4a02f2f4141fc605723dcee474a
廖weber <55630451+weberliao@users.noreply.github.com>
2019-11-22 01:09:53 +0800

    Update binary_search_tree_06170138.py

--- HW3/binary_search_tree_06170138.py
+++ HW3/binary_search_tree_06170138.py
@@ -1,1 +1,21 @@
-
+class TreeNode(object):
+	def _init_(self,x):
+		self.val =x
+		self.left = None
+		self.right = None
+		class Solution(object):
+			def insert(self ,root,val):
+				node1 = TreeNode(val)
+				if root.val:
+					if x<root.val:
+						if root.left is None :
+							root.left = TreeNode(x)
+					else:
+						root.left.insert(x)
+				elif x>=root.val:
+					if root.rigjt is None :
+						root.right = TreeNode(x)
+					else:
+						root.right.insert(x)
+				else:
+					root.val = x
