b4ef321f475b7bbd3619cff331bd05dc527421e8
廖weber <55630451+weberliao@users.noreply.github.com>
2019-12-06 17:57:24 +0800

    Update hash_table_06170138.py

--- HW4/hash_table_06170138.py
+++ HW4/hash_table_06170138.py
@@ -5,59 +5,45 @@ class ListNode:
         self.val = val
         self.next = None
 class MyHashSet:
-    def MD5(self,key):
-        h = MD5.new()
-        h.update(key.encode("utf-8")) 
-        h = int(h.hexdigest(),16)
-        return h
-  
     def __init__(self, capacity=5):
-        ##capacity的bucket
         self.capacity = capacity
         self.data = [None] * capacity
-    
-    def add(self, key):
         
-        number = int(MD5.new(key.encode("utf-8")).hexdigest(),16)
-        index = number % self.capacity
-        
+def add(self, key):
+        h = MD5.new()
+        h = int(temp,16)
+        index = h % self.capacity
         if self.data[index] == None:
-            self.data[index] = ListNode(number)
-            
+            self.data[index] = ListNode(temp)
+            return
         else:
-            new_node = ListNode(number)
-            new_node.next = self.data[index]
-            self.data[index] = new_node
-            
-      def remove(self, key):
-
-        while self.contains(key) != False:
-            number = int(MD5.new(key.encode("utf-8")).hexdigest(),16)
-            index = number % self.capacity
-            blank = self.data[index]
-            
-            if blank.val == number:
-                self.data[index] = blank.next
-                self.remove(key)
-                return
-            
-            pointer = None
-            while blank: 
-                if blank.val == number and blank.next:
-                    pointer.next = blank.next
-                    break
-                
-                elif blank.val == number and blank.next is None:
-                    pointer.next = None
-                    break
+            node = self.data[index]
+            while node:
+                if node.val == temp:
+                    return
+                if node.next == None:
+                    node.next = ListNode(temp)
+                    return
+                node = node.next
+def remove(self, key):
+        key_int = MyHashSet.Hash(self, key)
+        index = key_int % self.capacity
+        node_head = self.data[index]
+        if node_head:
+            if node_head.val == key_int :
+                node_head=node_head.next
+                self.data[index] = node_head
+        if node_head : 
+            while node_head.next:
+                if node_head.next.val == key_int:
+                    node_head.next = node_head.next.next
                     
-                elif blank.val != number:
-                    pointer = blank
-                    blank = blank.next
                     
-            self.remove(key)
+                else:
+                    node_head = node_head.next
             
 ##參考資料
 ##https://www.nosuchfield.com/2016/07/29/the-python-implementationp-of-HashTable/
 ##http://alrightchiu.github.io/SecondRound/hash-tableintrojian-jie.html
 ##https://www.geeksforgeeks.org/linked-list-set-3-deleting-node/
+##https://github.com/C-WeiYu/WeiYu/blob/master/HW4/hash_table_06170201.py
